e30eaab74665c55dd2e7632178956d75f9be49bf
t2023-m0125 <t2023-m0125@t2023-m0125ui-MacBookAir.local>
2024-08-13 19:32:52 +0900

    최종완성

---
+++ src/App.jsx
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("");
+  const [gold, setGold] = useState(0);
+  const [silver, setSilver] = useState(0);
+  const [bronze, setBronze] = useState(0);
+
+  const handleCountryChange = (event) => {
+    setCountry(event.target.value);
+  };
+
+  const handleGoldChange = (event) => {
+    setGold(Number(event.target.value));
+  };
+
+  const handleSilverChange = (event) => {
+    setSilver(Number(event.target.value));
+  };
+
+  const handleBronzeChange = (event) => {
+    setBronze(Number(event.target.value));
+  };
+
+  const handleAddCountry = (event) => {
+    event.preventDefault();
+    const existingCountry = countries.find((c) => c.name === country);
+    if (existingCountry) {
+      alert("이미 등록된 국가입니다.");
+      return;
+    }
+    const newCountry = {
+      name: country,
+      medals: { gold, silver, bronze },
+    };
+
+    setCountries([...countries, newCountry]);
+
+    setCountry("");
+    setGold(0);
+    setSilver(0);
+    setBronze(0);
+  };
+
+  const handleUpdateCountry = (event) => {
+    event.preventDefault();
+
+    const existingCountry = countries.find((c) => c.name === country);
+    if (existingCountry) {
+      const updatedCountries = countries.map((c) =>
+        c.name === country
+          ? {
+              name: country,
+              medals: { gold, silver, bronze },
+            }
+          : c
+      );
+
+      setCountries(updatedCountries);
+
+      setCountry("");
+      setGold(0);
+      setSilver(0);
+      setBronze(0);
+    } else {
+      alert("등록 되지 않은 국가입니다.");
+    }
+  };
+
+  const handleDeleteCountry = (countryName) => {
+    const updatedCountries = countries.filter((c) => c.name !== countryName);
+    setCountries(updatedCountries);
+  };
+
+  //메달 수에 따른 순위변경
+  const sortedCountries = [...countries].sort((a, b) => {
+    if (b.medals.gold !== a.medals.gold) {
+      return b.medals.gold - a.medals.gold; 
+    } else if (b.medals.silver !== a.medals.silver) {
+      return b.medals.silver - a.medals.silver; 
+    } else {
+      return b.medals.bronze - a.medals.bronze; 
+    }
+  });
+
+  return (
+    <div className="container">
+      <h1>2024 파리 올림픽</h1>
+      <form className="input-group">
+        <div className="input-row">
+          <label>국가명</label>
+          <input type="text" value={country} onChange={handleCountryChange} />
+        </div>
+        <div className="input-row">
+          <label>금메달</label>
+          <input type="number" value={gold} onChange={handleGoldChange} />
+        </div>
+        <div className="input-row">
+          <label>은메달</label>
+          <input type="number" value={silver} onChange={handleSilverChange} />
+        </div>
+        <div className="input-row">
+          <label>동메달</label>
+          <input type="number" value={bronze} onChange={handleBronzeChange} />
+        </div>
+        <div className="button-group">
+          <button type="submit" onClick={handleAddCountry}>
+            국가 추가
+          </button>
+          <button type="button" onClick={handleUpdateCountry}>
+            업데이트
+          </button>
+        </div>
+      </form>
+
+      {countries.length === 0 ? (
+        <p>아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>국가명</th>
+              <th>금메달</th>
+              <th>은메달</th>
+              <th>동메달</th>
+              <th>액션</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedCountries.map((country, index) => (
+              <tr key={index}>
+                <td>{country.name}</td>
+                <td>{country.medals.gold}</td>
+                <td>{country.medals.silver}</td>
+                <td>{country.medals.bronze}</td>
+                <td>
+                  <button type="button" onClick={() => handleDeleteCountry(country.name)}>삭제</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default App;
